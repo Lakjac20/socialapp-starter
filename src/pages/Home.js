fede5ed254d5285c00895b203d25ad5f38211ed0
Alyssia Brooke Hall <alyssia.lackey@gmial.com>
2020-09-14 15:50:41 -0500

    Alyssia and Kamal's Home page idea

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,165 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import LoginForm from "../components/loginForm/LoginForm";
 import RegistrationForm from "../components/registrationForm/RegistrationForm";
 import MessagesForm from "../components/messagesForm/MessagesForm";
 import Menu from "../components/menu/Menu";
 import { userIsNotAuthenticated } from "../redux/HOCs";
-
+import {
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBMask,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+  MDBBtn,
+  MDBView,
+  MDBContainer,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBFormInline,
+  MDBAnimation
+} from "mdbreact";
+import "./Home.css";
 class Home extends React.Component {
+  state = {
+    collapseID: ""
+  };
+  toggleCollapse = collapseID => () =>
+    this.setState(prevState => ({
+      collapseID: prevState.collapseID !== collapseID ? collapseID : ""
+    }));
   render() {
+    const overlay = (
+      <div
+        id="sidenav-overlay"
+        style={{ backgroundColor: "transparent" }}
+        onClick={this.toggleCollapse("navbarCollapse")}
+      />
+    );
     return (
-      <div className="Home">
-        <Menu />
-        <h2>The Strength of the 5th Power</h2>
-        <h3>Login Below</h3>
-        <LoginForm />
-        <h3>Register Below</h3>
-        <RegistrationForm />
-        <hr></hr>
-        <h4>Messages</h4>
-        <MessagesForm/>
+      <div id="classicformpage">
+        <Router>
+          <div>
+            <MDBNavbar dark expand="md" fixed="top">
+              <MDBContainer>
+                <MDBNavbarBrand>
+                  <strong className="white-text">The Power Of Fifth</strong>
+                </MDBNavbarBrand>
+                <MDBNavbarToggler
+                  onClick={this.toggleCollapse("navbarCollapse")}
+                />
+                <MDBCollapse
+                  id="navbarCollapse"
+                  isOpen={this.state.collapseID}
+                  navbar
+                >
+                  <MDBNavbarNav right>
+                    <MDBNavItem>
+                      <MDBFormInline waves>
+                        <div className="md-form my-0">
+                          <input
+                            className="form-control mr-sm-2"
+                            type="text"
+                            placeholder="Search"
+                            aria-label="Search"
+                          />
+                        </div>
+                      </MDBFormInline>
+                    </MDBNavItem>
+                  </MDBNavbarNav>
+                </MDBCollapse>
+              </MDBContainer>
+            </MDBNavbar>
+            {this.state.collapseID && overlay}
+          </div>
+        </Router>
+        <MDBView>
+          <MDBMask className="d-flex justify-content-center align-items-center gradient">
+            <MDBContainer>
+              <MDBRow>
+                <MDBAnimation
+                  type="fadeInLeft"
+                  delay=".3s"
+                  className="white-text text-center text-md-left col-md-6 mt-xl-5 mb-5"
+                >
+                  <h1 className="h1-responsive font-weight-bold">
+                    Sign up right now!
+                  </h1>
+                  <hr className="hr-light" />
+                  <h6 className="mb-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Rem repellendus quasi fuga nesciunt dolorum nulla magnam
+                    veniam sapiente, fugiat! Commodi sequi non animi ea dolor
+                    molestiae, quisquam iste, maiores. Nulla.
+                  </h6>
+                  <MDBBtn color="indigo">
+                    Sign Up
+                   </MDBBtn>
+                </MDBAnimation>
+                <MDBCol md="6" xl="5" className="mb-4">
+                  <MDBAnimation type="fadeInRight" delay=".3s">
+                    <MDBCard id="classic-card">
+                      <MDBCardBody className="white-text">
+                        <h3 className="text-center">
+                          <MDBIcon icon="user" /> Login:
+                        </h3>
+                        <hr className="hr-light" />
+                        <MDBInput
+                          className="white-text"
+                          iconClass="white-text"
+                          label="Username"
+                          icon="user"
+                        />
+                        <MDBInput
+                          className="white-text"
+                          iconClass="white-text"
+                          label="Password"
+                          icon="lock"
+                          type="password"
+                        />
+                        <div className="text-center mt-4 black-text">
+                          <MDBBtn color="indigo">Sign In</MDBBtn>
+                          <hr className="hr-light" />
+                          <div className="text-center d-flex justify-content-center white-label">
+                            <a href="#!" className="p-2 m-2">
+                              <MDBIcon
+                                fab
+                                icon="twitter"
+                                className="white-text"
+                              />
+                            </a>
+                            <a href="#!" className="p-2 m-2">
+                              <MDBIcon
+                                fab
+                                icon="linkedin"
+                                className="white-text"
+                              />
+                            </a>
+                            <a href="#!" className="p-2 m-2">
+                              <MDBIcon
+                                fab
+                                icon="instagram"
+                                className="white-text"
+                              />
+                            </a>
+                          </div>
+                        </div>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </MDBAnimation>
+                </MDBCol>
+              </MDBRow>
+            </MDBContainer>
+          </MDBMask>
+        </MDBView>
       </div>
     );
   }
 }
-
 export default userIsNotAuthenticated(Home);
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Menu from "../components/menu/Menu";
 import { userIsAuthenticated } from "../redux/HOCs";
-
+import MessagesForm from "../components/messagesForm/MessagesForm";
 class Profile extends React.Component {
   render() {
     return (
       <div className="Profile">
         <Menu isAuthenticated={this.props.isAuthenticated} />
         <h2>Profile</h2>
+        <h4>Messages</h4>
+        <MessagesForm/>
       </div>
     );
   }
