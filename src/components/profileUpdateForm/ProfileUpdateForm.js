7adb34528ae7a5e0d5c8990e65f78151abc1da88
kamaljitkaur220 <kamalgidda89@gmail.com>
2020-09-29 14:15:41 -0400

    git messages

--- src/components/profileUpdateForm/ProfileUpdateForm.js
+++ src/components/profileUpdateForm/ProfileUpdateForm.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Menu from "../menu/Menu";
+import { Link } from "react-router-dom";
 import { userIsAuthenticated } from "../../redux/HOCs";
 import "./ProfileUpdateForm.css";
 import DataService from "../../service/DataService";
+
+
+import { MDBBtn, MDBIcon } from "mdbreact";
 
 class ProfileUpdateForm extends React.Component {
   constructor(props) {
@@ -26,40 +30,69 @@ class ProfileUpdateForm extends React.Component {
   render() {
     const { loading, error } = this.props;
     return (
-      <div className="profileupdatepage">
-        <Menu isAuthenticated={this.props.isAuthenticated} />
-        <div>
-          <h1>Update Profile:</h1>
-          <form id="Registration-form" onSubmit={this.handleUpdate}>
-            <label htmlFor="about">About you</label>
-            <input
-              className="aboutyou"
-              type="text"
-              name="about"
-              autoFocus
-              required
-              onChange={this.handleChange}
-            />
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              required
-              onChange={this.handleChange}
-            />
-            <label htmlFor="displayName">Display Name</label>
-            <input
-              type="text"
-              name="displayName"
-              required
-              onChange={this.handleChange}
-            />
-            <button type="submit" disabled={loading}>
-              Register
-            </button>
-          </form>
-        </div>
+      <div className="Profile">
+      <div class="wrapper">
+  <div class="sidebar">
+     <img src="https://www.metal-archives.com/images/2/7/9/4/27946_logo.jpg"></img>
+      <ul>
+          <li><Link to="/Profile"><i class="fa fa-home"></i>Profile</Link></li>
+          <li><a href="#"><i class="fa fa-user"></i>Users</a></li>
+         
+          <li><Link to="/messagefeed"><MDBIcon class="fa fa-comment" far icon="comment" > </MDBIcon>Comments</Link></li>
+          
+          
+         
+         
+      </ul>
+     
+  </div>
+  <div class="main_content">
+      <div class="header">The Power Of Fith welcomes you! 
+      <Menu isAuthenticated={this.props.isAuthenticated} />
+      
       </div>
+      
+     
+      <div className="profileupdatepage">
+
+<div>
+  <h1>Update Profile:</h1>
+  <form id="Registration-form" onSubmit={this.handleUpdate}>
+    <label htmlFor="about">About you</label>
+    <input
+      className="aboutyou"
+      type="text"
+      name="about"
+      autoFocus
+      required
+      onChange={this.handleChange}
+    />
+    <label htmlFor="password">Password</label>
+    <input
+      type="password"
+      name="password"
+      required
+      onChange={this.handleChange}
+    />
+    <label htmlFor="displayName">Display Name</label>
+    <input
+      type="text"
+      name="displayName"
+      required
+      onChange={this.handleChange}
+    />
+    <button type="submit" disabled={loading}>
+      Register
+    </button>
+  </form>
+</div>
+</div>
+     
+  </div>
+</div>
+    </div>
+      
+     
     );
   }
 }
--- src/pages/MessageFeed.js
+++ src/pages/MessageFeed.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Menu from "../components/menu/Menu";
+import { Link } from "react-router-dom";
 import { userIsAuthenticated } from "../redux/HOCs";
 import DataService from "../service/DataService";
 import Message from "../components/message/Message";
+import "./Profile.css";
+import { MDBBtn,MDBIcon } from "mdbreact";
+import logo from "../assets/images/logo3.jpg";
 
 class MessageFeed extends React.Component {
   constructor(props) {
@@ -59,31 +63,62 @@ class MessageFeed extends React.Component {
     //   );
     // }
     return (
-      <div className="MessageBlock">
+      <div className="Profile">
+        <div class="wrapper">
+    <div class="sidebar">
+    <img src={logo} id="title" alt="logo"></img>
+        <ul>
+            <li ><Link to="/Profile"><i class="fa fa-home"></i>Profile</Link></li>
+            <li><a href="#"><i class="fa fa-user"></i>Users</a></li>
+           
+            <li><Link to="/messagefeed"><MDBIcon class="fa fa-comment" far icon="comment" > </MDBIcon>MessageFeed</Link></li>
+            
+            
+           
+           
+        </ul>
+      
+    </div>
+    <div class="main_content">
+        <div class="header">The Power Of five welcomes you! 
         <Menu isAuthenticated={this.props.isAuthenticated} />
-
-        <h1>Messages</h1>
-        <div className="message-field">
-          <div className="messages">
-            {this.state.messages.map((messageObject) => (
-              <Message key={messageObject.id} {...messageObject} />
-            ))}
-            {/* <MessageList
-              handleLike={this.handleLike}
-              messageArray={this.state.messages}
-            /> */}
-          </div>
-          <div className="NewMessage">
-            <form
-              value={this.state.message.text}
-              onChange={this.handleChange}
-              onSubmit={this.handleSubmit}
-            >
-              <input name="text" type="text" />
-              <button>Create Message</button>
-            </form>
-          </div>
+        
         </div>
+        
+        <div className="profilespace">
+        <h1>Profile</h1>
+        <div className="MessageBlock">
+
+<h3>Messages From Everyone</h3>
+<div className="message-field">
+  <div className="messages">
+    {this.state.messages.map((messageObject) => (
+      <Message key={messageObject.id} {...messageObject} />
+    ))}
+    {/* <MessageList
+      handleLike={this.handleLike}
+      messageArray={this.state.messages}
+    /> */}
+  </div>
+  <div className="NewMessage">
+    <form
+      value={this.state.message.text}
+      onChange={this.handleChange}
+      onSubmit={this.handleSubmit}
+    >
+      <input name="text" type="text" />
+      <button>Penny For Your Thoughts</button>
+    </form>
+  </div>
+</div>
+</div>
+          
+       
+        </div>
+        
+        
+    </div>
+</div>
       </div>
     );
   }
