2efa2efdb2459be7fc7629f5f338cdb56c11e049
Alyssia Brooke Hall <alyssia.lackey@gmial.com>
2020-10-02 08:41:46 -0500

    messages who knew

--- src/components/messages/messageList/MessageList.js
+++ src/components/messages/messageList/MessageList.js
@@ -1,1 +1,11 @@
 import MessageList from  "../../messages/messageList/MessageList.css";
+
+class MessageList extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        message: "",
+      };
+    }
+
+export 
--- src/pages/MessageFeed.js
+++ src/pages/MessageFeed.js
@@ -1,3 +1,10 @@
 
+class MessageFeed extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        message: "",
+      };
+    }
 
 export default userIsAuthenticated(MessageFeed);
